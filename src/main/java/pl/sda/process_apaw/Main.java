d3401acbd47195ccccdb556d14a21c4467c36fa9
Andrzej Pawlowski <panandy@poczta.onet.pl>
2019-03-02 12:46:09 +0100

    read and open works

--- src/main/java/pl/sda/process_apaw/Main.java
+++ src/main/java/pl/sda/process_apaw/Main.java
@@ -1,8 +1,39 @@
 package pl.sda.process_apaw;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Process process;
+
+        String homeDir = System.getProperty("user.home");
+        String homeOs = System.getProperty("os.arch");
+
+        System.out.println(homeDir);
+        System.out.println(homeOs);
+
+        try {
+            process = Runtime.getRuntime().exec("cmd /c dir " + homeDir);
+//            process = Runtime.getRuntime().exec("explorer");
+//            process = Runtime.getRuntime().exec("cmd /c notepad C:\\Users\\Andy Studio\\text.txt");
+            System.out.println(process.getOutputStream());
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+           // System.out.println(reader);
+            String line;
+            while((line = reader.readLine()) != null){
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
